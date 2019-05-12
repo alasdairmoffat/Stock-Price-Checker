188bfebffd2be23d29c9581d59a1c97b7b9c4af4
Alasdair Moffat <contact@alasdairmoffat.com>
2019-05-12 20:12:13 +0100

    Project completed, all tests passing

---
+++ routes/api.js
@@ -0,0 +1,95 @@
+/*
+ *
+ *
+ *       Complete the API routing below
+ *
+ *
+ */
+
+const mongoose = require('mongoose');
+const axios = require('axios');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+  })
+  .then(console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+const { Schema } = mongoose;
+
+const stockSchema = new Schema({
+  stock: {
+    type: String,
+    required: true,
+  },
+  likeIps: {
+    type: Array,
+    default: [],
+  },
+});
+
+const Stock = mongoose.model('Stock', stockSchema);
+
+async function queryStock(stockName, like, ip) {
+  const stock = stockName.toUpperCase();
+  const url = `https://api.iextrading.com/1.0/stock/${stockName}/price`;
+
+  const [dbResponse, stockPrice] = await Promise.all([
+    Stock.findOneAndUpdate(
+      { stock },
+      like
+        ? {
+          stock,
+          $addToSet: { likeIps: ip },
+        }
+        : {
+          stock,
+        },
+      {
+        new: true,
+        upsert: true,
+      },
+    ),
+    axios.get(url),
+  ]);
+
+  return {
+    stock: dbResponse.stock,
+    price: stockPrice.data,
+    likes: dbResponse.likeIps.length,
+  };
+}
+
+module.exports = (app) => {
+  app.route('/api/stock-prices').get(async (req, res) => {
+    const { stock, like } = req.query;
+    const { ip } = req;
+
+    if (Array.isArray(stock)) {
+      const [response1, response2] = await Promise.all([
+        queryStock(stock[0], like, ip),
+        queryStock(stock[1], like, ip),
+      ]);
+
+      const stockData = [response1, response2].map((response, i, array) => {
+        const { stock, price } = response;
+        const rel_likes = response.likes - array[(i + 1) % 2].likes;
+
+        return { stock, price, rel_likes };
+      });
+
+      res.json({
+        stockData,
+      });
+    } else {
+      const response = await queryStock(stock, like, ip);
+
+      res.json(response);
+    }
+  });
+};
---
+++ tests/2_functional-tests.js
@@ -0,0 +1,128 @@
+/*
+ *
+ *
+ *       FILL IN EACH FUNCTIONAL TEST BELOW COMPLETELY
+ *       -----[Keep the tests in the same order!]-----
+ *       (if additional are added, keep them at the very end!)
+ */
+
+/* global suite test */
+
+const chaiHttp = require('chai-http');
+const chai = require('chai');
+
+const { assert } = chai;
+const server = require('../server');
+
+chai.use(chaiHttp);
+
+suite('Functional Tests', () => {
+  suite('GET /api/stock-prices => stockData object', () => {
+    let numLikes1;
+
+    test('1 stock', (done) => {
+      chai
+        .request(server)
+        .get('/api/stock-prices')
+        .query({ stock: 'goog' })
+        .end((err, res) => {
+          // complete this one too
+          assert.equal(res.status, 200);
+          assert.property(res.body, 'stock', 'Response should contain the stock ticker.');
+          assert.property(res.body, 'price', 'Response should contain the price.');
+          assert.property(res.body, 'likes', 'Response should contain the number of likes.');
+          assert.equal(res.body.stock, 'GOOG', 'Response stock name should match.');
+          assert.isNumber(res.body.price, 'Stock price should be a number.');
+          assert.isNumber(res.body.likes, 'Number of likes should be a number.');
+
+          numLikes1 = res.body.likes;
+          done();
+        });
+    });
+
+    test('1 stock with like', (done) => {
+      chai
+        .request(server)
+        .get('/api/stock-prices')
+        .query({ stock: 'goog', like: true })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.property(res.body, 'stock', 'Response should contain the stock ticker.');
+          assert.property(res.body, 'price', 'Response should contain the price.');
+          assert.property(res.body, 'likes', 'Response should contain the number of likes.');
+          assert.equal(res.body.stock, 'GOOG', 'Response stock name should match.');
+          assert.isNumber(res.body.price, 'Stock price should be a number.');
+          assert.equal(res.body.likes, numLikes1 + 1, 'Number of likes should be incremented.');
+          done();
+        });
+    });
+
+    test('1 stock with like again (ensure likes arent double counted)', (done) => {
+      chai
+        .request(server)
+        .get('/api/stock-prices')
+        .query({ stock: 'goog', like: true })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.property(res.body, 'stock', 'Response should contain the stock ticker.');
+          assert.property(res.body, 'price', 'Response should contain the price.');
+          assert.property(res.body, 'likes', 'Response should contain the number of likes.');
+          assert.equal(res.body.stock, 'GOOG', 'Response stock name should match.');
+          assert.isNumber(res.body.price, 'Stock price should be a number.');
+          assert.equal(res.body.likes, numLikes1 + 1, 'Number of likes should not be incremented.');
+          done();
+        });
+    });
+
+    const stock = ['AAPL', 'MSFT'];
+    const numLikes2 = [];
+
+    test('2 stocks', (done) => {
+      chai
+        .request(server)
+        .get('/api/stock-prices')
+        .query({ stock })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.property(res.body, 'stockData');
+          assert.isArray(res.body.stockData, 'Response stockData should be an array.');
+          assert.equal(res.body.stockData.length, 2, 'Response stockData should be length 2.');
+
+          res.body.stockData.forEach((data, i) => {
+            assert.property(data, 'stock', 'Response should contain the stock ticker.');
+            assert.property(data, 'price', 'Response should contain the price.');
+            assert.property(data, 'rel_likes', 'Response should contain the number of likes.');
+            assert.equal(data.stock, stock[i], 'Response stock name should match.');
+            assert.isNumber(data.price, 'Stock price should be a number.');
+            assert.isNumber(data.rel_likes, 'Number of rel_likes should be a number.');
+
+            numLikes2[i] = data.rel_likes;
+          });
+          done();
+        });
+    });
+
+    test('2 stocks with like', (done) => {
+      chai
+        .request(server)
+        .get('/api/stock-prices')
+        .query({ stock, like: true })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.property(res.body, 'stockData');
+          assert.isArray(res.body.stockData, 'Response stockData should be an array.');
+          assert.equal(res.body.stockData.length, 2, 'Response stockData should be length 2.');
+
+          res.body.stockData.forEach((data, i) => {
+            assert.property(data, 'stock', 'Response should contain the stock ticker.');
+            assert.property(data, 'price', 'Response should contain the price.');
+            assert.property(data, 'rel_likes', 'Response should contain the number of likes.');
+            assert.equal(data.stock, stock[i], 'Response stock name should match.');
+            assert.isNumber(data.price, 'Stock price should be a number.');
+            assert.equal(data.rel_likes, numLikes2[i], 'Number of rel_likes should be unchanged.');
+          });
+          done();
+        });
+    });
+  });
+});
