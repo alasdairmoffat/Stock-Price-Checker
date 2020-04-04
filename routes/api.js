a5e1372eaab303c27e9e182b7179719fcb6e119c
Alasdair Moffat <contact@alasdairmoffat.com>
2020-04-04 14:36:42 +0100

    Security Fixes

--- routes/api.js
+++ routes/api.js
@@ -16,6 +16,7 @@ mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useFindAndModify: false,
+    useUnifiedTopology: true,
   })
   .then(console.log('MongoDB connected'))
   .catch(err => console.log(err));
